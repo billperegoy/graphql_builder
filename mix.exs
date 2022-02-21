c48d465f3d8a063b86559f6aaf90bfa4cdd48a32
thread <thread@threadbox.net>
2022-02-21 13:10:22 -0500

    bump version to 1.3.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GraphqlBuilder.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/billperegoy/graphql_builder"
-  @version "0.3.0"
+  @version "0.3.1"
 
   def project do
     [
